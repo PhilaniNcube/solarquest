1db2017a6465bc67a0548cafde989f43abc6e1f7
Philani Ncube <ncbphi001@gmail.com>
2022-08-19 21:33:58 +0200

    pseudoelements

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -5,21 +5,33 @@ import { FunctionComponent } from "react";
 const Hero : FunctionComponent = () => {
   return (
     <header className="px-4">
-      <div className="max-w-7xl mx-auto h-screen grid grid-cols-1 md:grid-cols-2">
+      <div className="max-w-7xl mx-auto py-16 grid grid-cols-1 md:grid-cols-2">
         <div className="h-full w-full flex flex-col items-start justify-center p-8">
           <h1 className="text-3xl md:text-5xl font-semibold text-red-600">
             How does life without Load Shedding Sound?
           </h1>
-          <p className="text-md md:text-lg lg:text-xl text-slate-700 mt-3 font-medium">
-            With more than enough backup power, with our packages you will always have electricity even if <span className="text-blue-600 font-bold">Eskom</span> dosen't!
+          <p className="text-md md:text-lg lg:text-xl leading-7 text-slate-700 mt-3 font-medium">
+            With more than enough backup power, with our packages you will
+            always have electricity even if{" "}
+            <span className="text-blue-600 font-bold">Eskom</span> dosen't!
           </p>
 
           <Link href="/contact">
-            <a className="px-8 py-2 rounded-full bg-red-600 mt-4 text-white text-xl">Learn more</a>
+            <a className="px-8 py-2 rounded-full bg-red-600 mt-4 text-white text-xl">
+              Learn more
+            </a>
           </Link>
         </div>
-        <div className="w-full py-24 h-full">
-          <Image src="/images/home_roof.jpg" alt="roof" width={1159} height={1500} priority quality={100}  className="rounded-lg h-[80%] my-auto w-10/12 mx-auto object-cover shadow-lg shadow-gray-800/40" />
+        <div className="w-full isolated py-24 h-full relative before:content-[''] before:absolute before:bottom-20 before:right-10 before:h-56 before:w-56 before:-z-10 before:rotate-45 before:bg-slate-300/30 before:rounded-lg before:shadow-2xl after:content-[''] after:absolute after:top-10 after:left-10 after:h-56 after:w-56 after:-z-10 after:rotate-45 after:bg-red-300/30 after:rounded-full after:shadow-xl">
+          <Image
+            src="/images/rebel_duo_plus.png"
+            alt="rebel duo plus"
+            width={1101}
+            height={895}
+            priority
+            quality={100}
+            className="rounded-lg h-[80%] my-auto w-10/12 mx-auto object-cover "
+          />
         </div>
       </div>
     </header>
