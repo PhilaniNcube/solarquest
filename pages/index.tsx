85d8b118e55c4f9f4cf44043352caacdd7bb99fb
Philani Ncube <ncbphi001@gmail.com>
2022-08-28 12:44:37 +0200

    keyword separator

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ const Home: NextPage = () => {
         />
         <meta
           name="keywords"
-          content="solar pwer solutions, solar services, solar panels, solar system design, lithium batteries, solar inverters, lithium ion battery, solar power, solar panel installation, solar inverter price, off grid, solar panel inverter, solar power panels, solar energy solutions, hybrid inverter, voltage switcher, ac combiner box, dc combiner box, solar installer, pv cable, solar cable, solar support, solar repairs, solar training, balance of systems "
+          content="solar pwer solutions; solar services; solar panels; solar system design; lithium batteries; solar inverters; lithium ion battery; solar power; solar panel installation; solar inverter price; off grid; solar panel inverter; solar power panels; solar energy solutions; hybrid inverter; voltage switcher; ac combiner box; dc combiner box; solar installer; pv cable; solar cable; solar support; solar repairs; solar training; balance of systems "
         />
       </Head>
       <Hero />
