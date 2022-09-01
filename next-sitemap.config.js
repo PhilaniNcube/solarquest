d19cb29d673cd4526811323ff0d013a6280d48c1
Philani Ncube <ncbphi001@gmail.com>
2022-09-01 17:53:36 +0200

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+const siteUrl = "https://www.solarquest.co.za"
+
+module.exports = {
+  siteUrl: siteUrl || "https://www.solarquest.co.za",
+  generateRobotsTxt: true, // (optional)
+  exclude: ["/profile"],
+
+};
